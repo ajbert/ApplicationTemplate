6138973f19144c68f2d1540f4cbe9c8ec0710111
ajbert <100375590+ajbert@users.noreply.github.com>
2022-09-21 13:54:25 -0500

    Add files via upload

--- Services/IFileService.cs
+++ Services/IFileService.cs
@@ -1,4 +1,6 @@
-﻿namespace ApplicationTemplate.Services;
+﻿using System.Collections.Generic;
+
+namespace ApplicationTemplate.Services;
 
 /// <summary>
 ///     This service interface only exists an example.
@@ -7,5 +9,11 @@
 public interface IFileService
 {
     void Read();
-    void Write();
+    void Write(int movieId, string movieTitle, string genresString);
+
+    void Display();
+
+    List<int> movieIds { get; set; }
+    List<string> movieTitles { get; set; }
+    List<string> movieGenres { get; set; }
 }
